af553a04843c91e6ce173a2fadee526776d9865f
SoulOfSet <jacobsabella@yahoo.com>
2016-03-20 20:02:05 -0500

    Starting up location data setting and preferencing

--- www/js/app.js
+++ www/js/app.js
@@ -8,9 +8,14 @@ app.controller("tabHostController", function($scope) {
 //search.html functions
 app.controller("searchController", function($scope){
     $scope.search = function(){
-        data.setSearchType("productID");
-        data.setLocation(63379, null)
-        data.search("0088392936007");
+        data.setSearchType("name");
+        data.setLocation(localStorage.getItem("latitude"), localStorage.getItem("longitude"));
+        data.search($('#txtSearch').val());
     }
 
 });
+
+//settings.html functions
+app.controller("settingsController", function($scope)){
+    $scope.
+}
