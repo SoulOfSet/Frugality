c05d4fd5656fd4a856b14f865e196fae0871ebf6
SoulOfSet <jacobsabella@yahoo.com>
2016-03-23 22:13:17 -0500

    Theme data

--- platforms/android/assets/www/js/app.js
+++ platforms/android/assets/www/js/app.js
@@ -1,12 +1,39 @@
+var data;
+
 //home.html functions
 app.controller("tabHostController", function($scope) {
-
+    if(localStorage.getItem("hasRun") == null){
+        ons.createDialog('intro_dialog.html').then(function(dialog) {
+            dialog.show();
+        });
+    }
 });
 
 //search.html functions
 app.controller("searchController", function($scope){
     $scope.search = function(){
-
+        data.setSearchType("name");
+        data.setLocation(localStorage.getItem("latitude"), localStorage.getItem("longitude"));
+        data.search($('#TxtSearch').val());
     }
 
+});
+
+//settings.html functions
+app.controller("settingsController", function($scope){
+
+});
+
+//intro_dialog.html functions
+app.controller("introDialogController", function($scope){
+   $scope.setLocationPref = function(button){
+       console.log("sawg");
+        if(button == 0){
+            $("#ChooseType").hide();
+            $("#ChooseZip").show();
+        }
+       else{
+            console.log("do me");
+        }
+   }
 });
