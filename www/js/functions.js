8c923e8129990e3018635d6d69ec2a6d02879725
SoulOfSet <jacobsabella@yahoo.com>
2016-03-24 12:52:32 -0500

    Allot of work to location preference selection. Pretty much done minus a couple minor things

--- www/js/functions.js
+++ www/js/functions.js
@@ -3,3 +3,25 @@
  * A js file for functions not relevant to the wrapper or any specific page
  */
 
+//update GPS data in local storage
+var updateGPSData = function(callback) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+        var lat = position.coords.latitude;
+        var long = position.coords.longitude;
+        localStorage.setItem("latitude", lat);
+        localStorage.setItem("longitude", long);
+        if (callback != null) {
+            callback(true); //We were able to triangulate GPS location
+        }
+    }, function() {
+        console.log('code: ' + error.code + '\n' + 'message: ' + error.message + '\n');
+        if (callback != null) {
+            callback(false); //GPS triangulation failed =/
+        }
+    });
+};
+
+//update zip code in local storage
+var updateZip = function(zip){
+    localStorage.setItem("zip", zip);
+}
