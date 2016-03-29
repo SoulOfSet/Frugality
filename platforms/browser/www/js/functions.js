46e170329d5bdc7e1f89da83fe0f2d5aea7873e9
soulofset <awesomewhitehot@yahoo.com>
2016-03-29 01:09:31 +0000

    changed the config.xml file

---
+++ platforms/browser/www/js/functions.js
@@ -0,0 +1,29 @@
+/**
+ * Created by SoulOfSet on 3/23/2016.
+ * A js file for functions not relevant to the wrapper or any specific page
+ */
+
+//update GPS data in local storage
+var updateGPSData = function(callback) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+        console.log("GPS Updated");
+        var lat = position.coords.latitude;
+        var long = position.coords.longitude;
+        localStorage.setItem("latitude", lat);
+        localStorage.setItem("longitude", long);
+        if (callback != null) {
+            console.log("swag");
+            callback(true); //We were able to triangulate GPS location
+        }
+    }, function() {
+        console.log('code: ' + error.code + '\n' + 'message: ' + error.message + '\n');
+        if (callback != null) {
+            callback(false); //GPS triangulation failed =/
+        }
+    });
+};
+
+//update zip code in local storage
+var updateZip = function(zip){
+    localStorage.setItem("zip", zip);
+};
